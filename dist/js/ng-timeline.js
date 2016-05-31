1cdbd300f0090b83cd47668f18a072b238dc7110
fmaturel <francoismaturel@gmail.com>
2016-05-31 16:16:37 +0200

    Fix demo timeline height

--- dist/js/ng-timeline.js
+++ dist/js/ng-timeline.js
@@ -163,7 +163,7 @@ angular.module('ngTimeline')
             }
           };
 
-          //########################################################################## TIMELINE OPTIONS
+          //########################################################################## TIMELINE DATA
           TL.Timeline.prototype.setData = function (data) {
             if (tl && data) {
               $log.debug('Loading timeline data: ', data);
